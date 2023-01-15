ed7d1403028148afbc1f91c5b27a27d067bb45f7
Richard Bernisca <taropogi_123@yahoo.com>
2023-01-15 23:42:27 +0800

    objects

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-if (true) {
-  const isTrue = true;
-}
+"use strict";
 
-console.log(isTrue); // ReferenceError
+// constructor functions
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+  this.age = 0;
+};
+
+// prototypes
+Person.prototype.calcAge = function () {
+  this.age = 2023 - this.birthYear;
+};
+
+const chad = new Person("Richard", 1993);
+
+chad.calcAge();
+console.log(chad.age);
